7823d520d47c7bb78ceb7e109b42f0141e9267ba
SabineZilde <sabine.zilde@protonmail.ch>
2021-12-23 23:37:18 +0200

    style: adding logo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import logo from '../Assets/Images/logo_pineapple.svg';
+import pineapple from '../Assets/Images/pineapple..svg';
 
 function Header() {
     return (
         <div>
-            pumpurum
+            <img src={logo} alt="pineapple logo" style={{marginTop: '50px', marginLeft: '50px'}}/>
+            <img src={pineapple} alt="pineapple" style={{marginLeft: '10px'}}/>
+
         </div>
     )
 }
