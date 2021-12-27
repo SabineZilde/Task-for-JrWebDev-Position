4f7fdd3a02d7802ddd6766994579d559cc40e5dc
SabineZilde <sabine.zilde@protonmail.ch>
2021-12-27 23:07:20 +0200

    feat: mobile submit button

--- src/Components/InputFiield/InputField.js
+++ src/Components/InputFiield/InputField.js
@@ -6,14 +6,17 @@ function InputField({ inputFieldText }) {
   const isSmallScreen = useMediaQuery({ query: "(max-width: 1000px)" });
 
   let inputStyle = "input_style_large input_style";
+  let submitStyle = "submit_style submit_position_large";
 
   if (isSmallScreen) {
     inputStyle = "input_style_small input_style";
-  }
+    submitStyle = "submit_style submit_position_small";
+  };
+  
   return (
     <div className="inline">
       <input type="text" placeholder={inputFieldText} className={inputStyle} />
-      <input type="submit" className="submit_style" value="->" />
+      <input type="submit" className={submitStyle} value="->" />
     </div>
   );
 }
