5dcdeba259565fe8257d21450ce887530d14aea4
SabineZilde <sabine.zilde@protonmail.ch>
2021-12-29 20:58:33 +0200

    Config mysql connection

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,28 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'sabine-zilde.magebithr.com',
+    password: 'BdCYihKIojDHwvd',
+    database: 'databaseName',
+});
+
+app.post('/create', (req, res) => {
+    const email = req.body.email;
+
+    db.query('INSERT INTO tablename (email) VALUES (?)',
+        [email],
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send('Values inserted')
+            }
+        }
+    );
+});
 
 app.listen(3002, () => {
     console.log('Server is up and running on port 3002');
