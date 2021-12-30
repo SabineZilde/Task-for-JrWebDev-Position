004a2a7fdd0e76a5518ca4de05b8f5f87f81670c
SabineZilde <sabine.zilde@protonmail.ch>
2021-12-30 23:19:32 +0200

    fix: adding media query for middle size

--- Client/src/App.js
+++ Client/src/App.js
@@ -6,12 +6,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useMediaQuery } from 'react-responsive';
 
 function App() {
-    const isSmallScreen = useMediaQuery({ query: "(max-width: 1000px)" });
+    const isSmallScreen = useMediaQuery({ query: "(max-width: 520px)" });
+    const isMidleScreen = useMediaQuery({ query: "(min-width: 520px)" });
+    const isLargeScreen = useMediaQuery({ query: "(min-width: 1000px)" });
 
     let background = "background_img";
 
     if (isSmallScreen) {
         background = "background_img_small";
+    };
+
+    if (isMidleScreen) {
+        background = 'background_img_middle';
+    };
+
+    if (isLargeScreen) {
+        background = 'background_img';
     };
 
     return (
